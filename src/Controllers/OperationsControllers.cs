82a8a65144c7f9aaca01a3c418b3777920209537
Gabriel Girbacea <gabriel.girbacea@gmail.com>
2021-01-29 18:35:40 +0200

    Functional APIs.
    Functional APIs.

---
+++ src/Controllers/OperationsControllers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsControllers : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public OperationsControllers(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)configuration;
+
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
